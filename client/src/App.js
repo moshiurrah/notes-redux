711d9e5ef0d648d530296f06fd154f9a8acc6a9a
Tanzim Mokammel <mtanzim@gmail.com>
2018-02-05 08:11:16 +0000

    started repurposing front end files to integrate existing notes app

--- client/src/App.js
+++ client/src/App.js
@@ -3,17 +3,10 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  state = {recipes: []}
   
   componentDidMount() {
-    fetch('/testReact')
-      .then(res => res.json())
-      .then(recipes => this.setState({ recipes }));
-      
-    //console.log(this.state.recipes);
   }
   render() {
-    console.log(this.state.recipes);
     return (
       <div className="App">
         <header className="App-header">
@@ -23,15 +16,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <h1>Recipes</h1>
-        {this.state.recipes.map(recipe =>
-          <div key={recipe._id}>
-            <h4>{recipe.title}</h4>
-            {recipe.ingredients.map (ing => 
-              <div key={ing._id}>{ing.title} {ing.qty} {ing.unit}</div>
-            )}
-          </div>
-        )}
+        <h1>App</h1>
       </div>
     );
   }
--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import './index.css';
-import RecipeApp from './RecipeApp';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<RecipeApp />, document.getElementById('root'));
-//registerServiceWorker();
+ReactDOM.render(<App/>, document.getElementById('root'));
+registerServiceWorker();
